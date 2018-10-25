e68abdb5117edced1a9f4d79759c70fa14055eac
Sandro Rohan <deansantos183@gmail.com>
2018-10-24 22:55:54 -0200

    Projeto iniciado, mostrando a mensagem Olá Mundo

---
+++ Teste/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo");
+
+            Console.ReadLine();
+        }
+    }
+}
